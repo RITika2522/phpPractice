75950ba3402b2690dd827f33e280786443668bb9
RITika2522 <ritikachandak2502@gmail.com>
2025-03-18 09:21:29 +0530

    Basic maths operations

---
+++ mathop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num1 = 20;
+        $num2 = 10;
+        $add = $num1 + $num2;
+        echo "Addition is: ",$add;
+        $sub = $num1 - $num2;
+        echo "Substraction is: ",$sub;
+    ?>
+</body>
+</html>
