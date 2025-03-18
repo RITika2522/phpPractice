ea1a67a57e35312c6058798678a79f05774ba6dc
RITika2522 <ritikachandak2502@gmail.com>
2025-03-18 09:31:24 +0530

    coumpound maths operation

---
+++ compoundmathop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num2 = 100;
+        $num2 += 20;
+        echo "Addition is: ",$num2;
+        $num2 -= 20;
+        echo "<br/>Substraction is: ",$num2;
+        $num2 *= 20;
+        echo "<br/>Multiplication is: ",$num2;
+    ?>
+</body>
+</html>
