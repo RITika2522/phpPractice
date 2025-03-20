43291bf6b6f5997d826b4ffe6012b64bf0a1b8f3
RITika2522 <ritikachandak2502@gmail.com>
2025-03-20 21:42:40 +0530

    conditional statement

---
+++ conditional.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF ELSE STATEMENT</title>
+</head>
+<body>
+    <?php
+        /* $age = 16;
+        if($age >= 18)
+            echo "eligible for driving";
+        else
+            echo "not eligible for driving"; */
+
+
+            
+        /* $d = Date("D");
+        if($d == "Fri")
+            echo "Have a nice weekend!";
+        else
+            echo "Have a nice day!"; */
+    ?> 
+    
+</body>
+</html>
