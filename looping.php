8675bbb81a1fdec4bbd2c2de39d27f874fa91125
RITika2522 <ritikachandak2502@gmail.com>
2025-03-25 09:25:27 +0530

    looping statements

---
+++ looping.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do While Loop</title>
+</head>
+<body>
+    <?php
+        
+
+        /* 
+        // print divisible by 3 and 5
+        $i =0;
+        do{
+            if($i%3==0 && $i%5==0){
+                echo("i = $i<br>");
+            }
+            $i++;
+            }while($i < 50);
+         */
+
+
+        /*
+        // print 1 to 10
+        $i = 0;
+        $num = 0;
+        do{
+            $i++;
+            echo("i= $i <br>");
+        }while($i < 10);
+        echo("loop stopped at i = $i"); 
+        */
+    ?>
+</body>
+</html>
