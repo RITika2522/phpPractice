a7122a1de3ac88a8ed3a3e7994b591c95a98d19e
RITika2522 <ritikachandak2502@gmail.com>
2025-04-01 09:15:59 +0530

    numeric array

--- array.php
+++ array.php
@@ -22,6 +22,29 @@
         foreach($numbers as $value){
             echo "Value is $value <br/>";
         }
+
+        // Assocative array
+        // Associative array with index
+        $salaries = array("John" => 1000, "Jane" => 2000, "Doe" => 3000);
+        echo "salary of John is = $salaries[John] <br/>";
+        echo "salary of Jane is = $salaries[Jane] <br/>";
+        echo "salary of Doe is = $salaries[Doe] <br/>";
+
+        // Associative array with foreach loop
+        $salaries = array("John" => 1000, "Jane" => 2000, "Doe" => 3000);
+        foreach($salaries as $name => $salary){
+            echo "$name earns $salary <br/>";
+        }
+
+        // Associative array with index
+        $salaries ['John'] = 1000;
+        $salaries ['Jane'] = 2000;
+        $salaries ['Doe'] = 3000;
+
+        echo "Salary of John is = $salaries['John'] <br/>";
+        echo "Salary of Jane is = $salaries['Jane'] <br/>";
+        echo "Salary of Doe is = $salaries['Doe'] <br/>";
+
     ?>
 </body>
 </html>
